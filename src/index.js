b356786d7b440d742a3d6a3274ec90ede039d321
tianaFomicheva <tianirika@gmail.com>
2021-02-21 21:17:21 +0500

    минификаторы, babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {config} from './modules/config'
 import AppService from './modules/app.service'
 import './modules/header.component'
+import  './css/index.css'
 
 console.log('config key:', config.key)
